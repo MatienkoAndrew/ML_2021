07d9e45a78af48a8fa7b80416dee2861e7b94589
MatienkoAndrew <matienko1999@bk.ru>
2021-05-05 19:55:49 +0300

    New threads

--- Diplom/toggle_menu/main.py
+++ Diplom/toggle_menu/main.py
@@ -27,42 +27,55 @@ class MainWindow(QMainWindow):
 
         # Смена значений
         ##-- меняется значение в ComboBox - перестраивается график
-        self.ui.comboBox.currentIndexChanged.connect(lambda: self.thread_plot_volatility.start())#lambda: UIFunctions.plot_change_perc(self))
+        self.ui.comboBox.currentIndexChanged.connect(lambda: self.thread_plot_volatility.start())
         self.ui.comboBox.currentIndexChanged.connect(lambda: UIFunctions.plot_change_perc_finish_thread(self))
 
         ##-- меняется значение в Short - перестраивается график
         self.ui.spinBox_short.valueChanged.connect(self.spinBox_short_changed)
         self.ui.horizontalSlider_short.valueChanged.connect(self.slider_short_changed)
-        self.ui.spinBox_long.valueChanged.connect(lambda: UIFunctions.plot_simple_strategy(self))
+        self.ui.spinBox_long.valueChanged.connect(self.spinBox_long_changed)
         self.ui.horizontalSlider_long.valueChanged.connect(self.slider_long_changed)
         ##-- меняется значение в spinBox_fbprophet - перестраивается график
-        self.ui.button_fb.clicked.connect(lambda: Stocker.create_prophet_model(self,
-                                                                               days=self.ui.spinBox_fbprophet.value()))
+        self.ui.button_fb.clicked.connect(self.spinBox_fb_changed)
 
+        ## Потоки
         self.thread_plot_stock = PlotStock(mainwindow=self)
         self.thread_plot_volatility = PlotVolatility(mainwindow=self)
+        self.thread_simple_strategy = PlotSimpleStrategy(mainwindow=self)
+        self.thread_plot_fbprophet = PlotFBProphet(mainwindow=self)
 
         # TOGGLE MENU
         self.ui.Btn_Toggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 250, True))
-        self.ui.btnFind.clicked.connect(self.threads_start)#lambda: UIFunctions.plot_stock(self))
+        self.ui.btnFind.clicked.connect(self.threads_start)
         self.ui.btnFind.clicked.connect(lambda: UIFunctions.plot_stock_when_finish_thread(self))
         self.ui.Btn_page_1.clicked.connect(lambda: UIFunctions.plot_stock(self))
-
-        # self.ui.Btn_page_1.clicked.connect(lambda: UIFunctions.plot_stock(self))
         self.ui.Btn_page_2.clicked.connect(lambda: UIFunctions.plot_change_perc(self))
         self.ui.Btn_page_3.clicked.connect(lambda: UIFunctions.plot_simple_strategy(self))
-        self.ui.Btn_page_4.clicked.connect(lambda: Stocker.create_prophet_model(self, days=180))
+        self.ui.Btn_page_4.clicked.connect(lambda: UIFunctions.plot_fbprophet(self))
         self.show()
 
+    ## Запуск всех потоков нажатием кнопки "Find"
     def threads_start(self):
         self.thread_plot_stock.start()
         self.thread_plot_volatility.start()
+        self.thread_simple_strategy.start()
+        self.thread_plot_fbprophet.start()
 
     def spinBox_short_changed(self):
         try:
             new_short_value = self.ui.spinBox_short.value()
             self.ui.horizontalSlider_short.setValue(new_short_value)
-            UIFunctions.plot_simple_strategy(self)
+            self.thread_simple_strategy.start()
+            UIFunctions.plot_simple_strategy_finish_thread(self)
+        except Exception as e:
+            print(e)
+
+    def spinBox_long_changed(self):
+        try:
+            new_long_value = self.ui.spinBox_long.value()
+            self.ui.horizontalSlider_long.setValue(new_long_value)
+            self.thread_simple_strategy.start()
+            UIFunctions.plot_simple_strategy_finish_thread(self)
         except Exception as e:
             print(e)
 
@@ -82,8 +95,9 @@ class MainWindow(QMainWindow):
         try:
             new_value = self.ui.spinBox_fbprophet.value()
             self.ui.spinBox_fbprophet.setValue(new_value)
-            time.sleep(2)
-            Stocker.create_prophet_model(self, days=new_value)
+            self.thread_plot_fbprophet.days = new_value
+            self.thread_plot_fbprophet.start()
+            UIFunctions.plot_fbprophet_finish_thread(self)
         except Exception as e:
             print(e)
 
--- Diplom/toggle_menu/ui_functions.py
+++ Diplom/toggle_menu/ui_functions.py
@@ -14,6 +14,7 @@ from stocker import Stocker
 
 from PyQt5.QtCore import QThread
 import time
+import fbprophet
 
 
 class PlotStock(QThread):
@@ -86,6 +87,157 @@ class PlotVolatility(QThread):
             )
 
 
+class PlotSimpleStrategy(QThread):
+    def __init__(self, mainwindow, parent=None):
+        super(PlotSimpleStrategy, self).__init__(parent)
+        self.mainwindow = mainwindow
+        self.fig_strategy = None
+
+    # простая стратегия
+    def simple_trade_strategy(self, company, short_window=40, long_window=100):
+        short_window = short_window
+        long_window = long_window
+
+        signals = pd.DataFrame(index=company.index)
+        signals['signal'] = 0.0
+        signals['short_mavg'] = company['Adj Close'].rolling(window=short_window, min_periods=1,
+                                                             center=False).mean()
+        signals['long_mavg'] = company['Adj Close'].rolling(window=long_window, min_periods=1, center=False).mean()
+
+        signals['signal'][short_window:] = np.where(
+            signals['short_mavg'][short_window:] > signals['long_mavg'][short_window:], 1.0, 0.0)
+        signals['positions'] = signals['signal'].diff()
+        return signals
+
+    def run(self):
+        textboxValue = self.mainwindow.ui.lineEdit.text()
+        if textboxValue != '':
+            stock = pdr.get_data_yahoo(textboxValue,
+                                       start=datetime.datetime(2006, 10, 1),
+                                       end=datetime.datetime.now())
+
+            short_window = self.mainwindow.ui.spinBox_short.value()
+            self.mainwindow.ui.horizontalSlider_short.setValue(short_window)
+            long_window = self.mainwindow.ui.spinBox_long.value()
+            self.mainwindow.ui.horizontalSlider_long.setValue(long_window)
+            try:
+                signals_company = self.simple_trade_strategy(stock, short_window, long_window)
+                raw_symbols = SymbolValidator().values
+                marker_triangle_up = raw_symbols[raw_symbols.index('triangle-up')]
+                marker_triangle_down = raw_symbols[raw_symbols.index('triangle-down')]
+                self.fig_strategy = go.Figure()
+                self.fig_strategy.add_trace(
+                    go.Scatter(
+                        x=stock.index,
+                        y=stock['Adj Close'],
+                        mode='lines',
+                        # line=dict(width=4),
+                        name='Adj Close'
+                    ))
+                self.fig_strategy.add_trace(
+                    go.Scatter(
+                        x=signals_company.index,
+                        y=signals_company['short_mavg'],
+                        mode='lines',
+                        name='short_mavg'
+                    ))
+                self.fig_strategy.add_trace(
+                    go.Scatter(
+                        x=signals_company.index,
+                        y=signals_company['long_mavg'],
+                        mode='lines',
+                        name='long_mavg'
+                    ))
+                self.fig_strategy.add_trace(
+                    go.Scatter(
+                        x=signals_company.loc[signals_company.positions == 1.0].index,
+                        y=signals_company.short_mavg[signals_company.positions == 1.0],
+                        marker_symbol=marker_triangle_up,
+                        mode='markers',
+                        marker_size=10,
+                        marker_color="yellowgreen",
+                        name='buy'
+                    ))
+                self.fig_strategy.add_trace(
+                    go.Scatter(
+                        x=signals_company.loc[signals_company.positions == -1.0].index,
+                        y=signals_company.short_mavg[signals_company.positions == -1.0],
+                        marker_symbol=marker_triangle_down,
+                        mode='markers',
+                        marker_size=10,
+                        marker_color="black",
+                        name='sell'
+                    ))
+                self.fig_strategy.update_layout(
+                    title=textboxValue + " simple strategy",
+                    xaxis_title='Date',
+                    yaxis_title='Price ($)'
+                )
+            except Exception as e:
+                print(e)
+
+
+class PlotFBProphet(QThread):
+    def __init__(self, mainwindow, parent=None, days=180):
+        super(PlotFBProphet, self).__init__(parent)
+        self.mainwindow = mainwindow
+        self.fig_strategy = None
+        self.days = days
+
+    def run(self):
+        model = fbprophet.Prophet(
+            daily_seasonality=False,
+            weekly_seasonality=False,
+            yearly_seasonality=True,
+            changepoint_prior_scale=0.05,
+            changepoints=None,
+        )
+        model.add_seasonality(name="monthly", period=30.5, fourier_order=5)
+        stock = pdr.get_data_yahoo(self.mainwindow.ui.lineEdit.text(), start=datetime.datetime(2006, 10, 1),
+                                   end=datetime.datetime.now()).reset_index()
+        stock["ds"] = stock["Date"]
+        stock["y"] = stock["Adj Close"]
+        max_date = max(stock["Date"])
+        # Fit on the stock history for self.training_years number of years
+        stock_history = stock[
+            stock["Date"]
+            > (max_date - pd.DateOffset(years=3))
+            ]
+
+        model.fit(stock_history)
+
+        # Make and predict for next year with future dataframe
+        future = model.make_future_dataframe(periods=self.days, freq="D")
+        future = model.predict(future)
+
+        title = "%s Historical and Predicted Stock Price" % self.mainwindow.ui.lineEdit.text()
+
+        ##-- plotly
+        self.fig_model = go.Figure([
+            go.Scatter(x=stock_history["ds"],
+                       y=stock_history["y"],
+                       mode="lines", opacity=0.8,
+                       name="Observations", line=dict(width=1.4, color='Black')),
+            go.Scatter(x=future["ds"], y=future["yhat"],
+                       name="Modeled", mode="lines", line=dict(width=2.4, color='Green')),
+            go.Scatter(name="Upper Bound", x=future["ds"], y=future["yhat_upper"],
+                       mode="lines", showlegend=False, line=dict(color='Green')),
+            go.Scatter(name="Lower Bound", x=future["ds"], y=future["yhat_lower"],
+                       mode="lines", fillcolor='rgba(67,255,1, 0.3)',
+                       fill='tonexty', showlegend=False, line=dict(color='Green'))
+        ])
+        self.fig_model.update_layout(
+            title=title,
+            xaxis_title="Date",
+            yaxis_title="Price",
+            font=dict(
+                family="Courier New, monospace",
+                size=14,
+                color="RebeccaPurple"
+            )
+        )
+
+
 class UIFunctions(MainWindow):
     def toggleMenu(self, maxWidth, enable):
         if enable:
@@ -125,93 +277,21 @@ class UIFunctions(MainWindow):
         self.ui.browser.setHtml(self.thread_plot_volatility.fig_vol.to_html(include_plotlyjs='cdn'))
         self.ui.stackedWidget.setCurrentWidget(self.ui.browser)
 
-    # простая стратегия
-    def simple_trade_strategy(self, company, short_window=40, long_window=100):
-        short_window = short_window
-        long_window = long_window
-
-        signals = pd.DataFrame(index=company.index)
-        signals['signal'] = 0.0
-        signals['short_mavg'] = company['Adj Close'].rolling(window=short_window, min_periods=1, center=False).mean()
-        signals['long_mavg'] = company['Adj Close'].rolling(window=long_window, min_periods=1, center=False).mean()
-
-        signals['signal'][short_window:] = np.where(
-            signals['short_mavg'][short_window:] > signals['long_mavg'][short_window:], 1.0, 0.0)
-        signals['positions'] = signals['signal'].diff()
-        return signals
+    def plot_simple_strategy_finish_thread(self):
+        self.thread_simple_strategy.finished.connect(lambda: UIFunctions.plot_simple_strategy(self))
 
     def plot_simple_strategy(self):
         UIFunctions.plot_simple_strategy_visible(self)
-        textboxValue = self.ui.lineEdit.text()
-        if textboxValue != '':
-            stock = pdr.get_data_yahoo(textboxValue,
-                                       start=datetime.datetime(2006, 10, 1),
-                                       end=datetime.datetime.now())
+        self.ui.browser.setHtml(self.thread_simple_strategy.fig_strategy.to_html(include_plotlyjs='cdn'))
+        self.ui.stackedWidget.setCurrentWidget(self.ui.browser)
 
-            short_window = self.ui.spinBox_short.value()
-            self.ui.horizontalSlider_short.setValue(short_window)
-            long_window = self.ui.spinBox_long.value()
-            self.ui.horizontalSlider_long.setValue(long_window)
-            try:
-                signals_company = UIFunctions.simple_trade_strategy(self, stock, short_window, long_window)
-                raw_symbols = SymbolValidator().values
-                marker_triangle_up = raw_symbols[raw_symbols.index('triangle-up')]
-                marker_triangle_down = raw_symbols[raw_symbols.index('triangle-down')]
-                fig_strategy = go.Figure()
-                fig_strategy.add_trace(
-                    go.Scatter(
-                        x=stock.index,
-                        y=stock['Adj Close'],
-                        mode='lines',
-                        # line=dict(width=4),
-                        name='Adj Close'
-                    ))
-                fig_strategy.add_trace(
-                    go.Scatter(
-                        x=signals_company.index,
-                        y=signals_company['short_mavg'],
-                        mode='lines',
-                        name='short_mavg'
-                    ))
-                fig_strategy.add_trace(
-                    go.Scatter(
-                        x=signals_company.index,
-                        y=signals_company['long_mavg'],
-                        mode='lines',
-                        name='long_mavg'
-                    ))
-                fig_strategy.add_trace(
-                    go.Scatter(
-                        x=signals_company.loc[signals_company.positions == 1.0].index,
-                        y=signals_company.short_mavg[signals_company.positions == 1.0],
-                        marker_symbol=marker_triangle_up,
-                        mode='markers',
-                        marker_size=10,
-                        marker_color="yellowgreen",
-                        name='buy'
-                    ))
-                fig_strategy.add_trace(
-                    go.Scatter(
-                        x=signals_company.loc[signals_company.positions == -1.0].index,
-                        y=signals_company.short_mavg[signals_company.positions == -1.0],
-                        marker_symbol=marker_triangle_down,
-                        mode='markers',
-                        marker_size=10,
-                        marker_color="black",
-                        name='sell'
-                    ))
-                fig_strategy.update_layout(
-                    title=textboxValue + " simple strategy",
-                    xaxis_title='Date',
-                    yaxis_title='Price ($)'
-                )
-                # fig.show()
+    def plot_fbprophet_finish_thread(self):
+        self.thread_plot_fbprophet.finished.connect(lambda: UIFunctions.plot_fbprophet(self))
 
-                self.ui.browser.setHtml(fig_strategy.to_html(include_plotlyjs='cdn'))
-                self.ui.stackedWidget.setCurrentWidget(self.ui.browser)
-            except Exception as e:
-                print(e)
-
+    def plot_fbprophet(self):
+        UIFunctions.plot_fbprophet_visible(self)
+        self.ui.browser.setHtml(self.thread_plot_fbprophet.fig_model.to_html(include_plotlyjs='cdn'))
+        self.ui.stackedWidget.setCurrentWidget(self.ui.browser)
 
     def simple_strategy_utils_not_visible(self):
         self.ui.lineEdit_long.setVisible(False)
@@ -246,9 +326,14 @@ class UIFunctions(MainWindow):
         self.ui.button_fb.setVisible(False)
         UIFunctions.simple_strategy_utils_visible(self)
 
-
-
-
-
-
-
+    def plot_fbprophet_visible(self):
+        self.ui.lineEdit_long.setVisible(False)
+        self.ui.spinBox_long.setVisible(False)
+        self.ui.horizontalSlider_long.setVisible(False)
+        self.ui.lineEdit_short.setVisible(False)
+        self.ui.spinBox_short.setVisible(False)
+        self.ui.horizontalSlider_short.setVisible(False)
+        self.ui.spinBox_fbprophet.setVisible(False)
+        self.ui.comboBox.setVisible(False)
+        self.ui.button_fb.setVisible(True)
+        self.ui.spinBox_fbprophet.setVisible(True)
